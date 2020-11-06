c7a18847a688ebc6bce05dae8926962a3b8e39fd
Alexandre CONRAD <alexandre.conrad2@etu.univ-lorraine.fr>
2020-11-06 17:41:02 +0100

    Début de création des tables de la base de données

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,42 @@
-DROP TABLE IF EXISTS billionaires;
+DROP TABLE IF EXISTS surveys;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS votes;
 
-CREATE TABLE billionaires (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL,
-  career VARCHAR(250) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS surveys
+(
+    `id`                        INT(11) PRIMARY KEY,
+    `name`                      VARCHAR(65),
+    `description`               TEXT(255),
+    `isAvailable`               BOOLEAN,
+    `endDate`                   DATETIME
 );
 
-INSERT INTO billionaires (first_name, last_name, career) VALUES
-  ('Aliko', 'Dangote', 'Billionaire Industrialist'),
-  ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
-  ('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
+CREATE TABLE IF NOT EXISTS comments
+(
+    `id`                        INT(11) PRIMARY KEY,
+    `id_survey`                 INT(11),
+    `message`                   TEXT(255)
+);
+
+CREATE TABLE IF NOT EXISTS votes
+(
+    `id`                        INT(11) PRIMARY KEY,
+    `id_survey`                 INT(11),
+    `option`                    DATETIME,
+    `availability`              VARCHAR(65),
+    `name`                      VARCHAR(65)
+);
+
+INSERT INTO surveys VALUES (1, 'Mon premier sondage',  'Mon tout premier sondage !', true, '2020-11-06 12:35:45');
+
+INSERT INTO comments VALUES (1, 1,  'Comptez sur moi :)');
+
+INSERT INTO votes VALUES (1, 1,  '2020-11-08 12:00:00', 'available', 'Alexandre');
+INSERT INTO votes VALUES (2, 1,  '2020-11-08 12:00:00', 'available', 'Clément');
+INSERT INTO votes VALUES (3, 1,  '2020-11-08 12:00:00', 'unknown', 'Liam');
+INSERT INTO votes VALUES (4, 1,  '2020-11-08 12:00:00', 'unavailable', 'Vincent');
+
+INSERT INTO votes VALUES (5, 1,  '2020-11-15 12:00:00', 'unknown', 'Alexandre');
+INSERT INTO votes VALUES (6, 1,  '2020-11-15 12:00:00', 'unknown', 'Clément');
+INSERT INTO votes VALUES (7, 1,  '2020-11-15 12:00:00', 'unknown', 'Liam');
+INSERT INTO votes VALUES (8, 1,  '2020-11-15 12:00:00', 'unavailable', 'Vincent');
