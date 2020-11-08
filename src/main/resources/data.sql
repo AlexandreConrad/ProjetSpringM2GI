49eb72b8d233b0470b31bb3f61caa37b4b5ec75e
Alexandre CONRAD <alexandre.conrad2@etu.univ-lorraine.fr>
2020-11-08 14:45:14 +0100

    Fin de création de la base de données

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,30 +1,30 @@
-DROP TABLE IF EXISTS surveys;
 DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS votes;
+DROP TABLE IF EXISTS surveys;
 
 CREATE TABLE IF NOT EXISTS surveys
 (
-    `id`                        INT(11) PRIMARY KEY,
-    `name`                      VARCHAR(65),
-    `description`               TEXT(255),
-    `isAvailable`               BOOLEAN,
-    `endDate`                   DATETIME
+    id_survey                   INT(11) PRIMARY KEY,
+    name                        VARCHAR(65) NOT NULL,
+    description                 TEXT(255) NOT NULL,
+    isAvailable                 BOOLEAN NOT NULL,
+    endDate                     DATETIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS comments
 (
-    `id`                        INT(11) PRIMARY KEY,
-    `id_survey`                 INT(11),
-    `message`                   TEXT(255)
+    id_comments                 INT(11) PRIMARY KEY,
+    id_survey                   INT(11) NOT NULL REFERENCES SURVEYS (id_survey),
+    message                     TEXT(255)
 );
 
 CREATE TABLE IF NOT EXISTS votes
 (
-    `id`                        INT(11) PRIMARY KEY,
-    `id_survey`                 INT(11),
-    `option`                    DATETIME,
-    `availability`              VARCHAR(65),
-    `name`                      VARCHAR(65)
+    id_votes                    INT(11) PRIMARY KEY NOT NULL,
+    id_survey                   INT(11) NOT NULL REFERENCES SURVEYS (id_survey),
+    option                      DATETIME NOT NULL,
+    answers                     VARCHAR(65),
+    name                        VARCHAR(65)
 );
 
 INSERT INTO surveys VALUES (1, 'Mon premier sondage',  'Mon tout premier sondage !', true, '2020-11-06 12:35:45');
