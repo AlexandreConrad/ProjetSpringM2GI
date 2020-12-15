f79bf343b9c0672cfe4a04d10a7cd7101b323d24
Alexandre CONRAD <alexandre.conrad2@etu.univ-lorraine.fr>
2020-12-15 10:18:12 +0100

    Architecture des services

---
+++ src/main/java/io/swagger/service/ChoiceService.java
@@ -0,0 +1,14 @@
+package io.swagger.service;
+
+import io.swagger.api.SurveysApiController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Service pour toutes les requêtes BDD en liaison "Choices"
+ */
+public class ChoiceService {
+
+    private static final Logger log = LoggerFactory.getLogger(SurveysApiController.class);
+    //TODO Faire toutes les fonctions
+}
