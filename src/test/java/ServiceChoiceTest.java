b4558c883af7d83e66726aa596af1e7c913cae71
Clément Chevalet <clement.chevalet@gmail.com>
2021-01-08 16:32:47 +0100

    fonctions de test sur les choices

---
+++ src/test/java/ServiceChoiceTest.java
@@ -0,0 +1,63 @@
+import io.swagger.model.Choice;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import io.swagger.model.Comment;
+import io.swagger.model.Survey;
+import io.swagger.service.ChoiceService;
+import io.swagger.service.CommentService;
+import io.swagger.service.SurveyService;
+import org.junit.*;
+
+public class ServiceChoiceTest {
+
+    /**
+     * Fonction getChoiceById
+     * Retourne tous les choix d'un sondage.
+     */
+    @Test
+    public void getChoiceById() {
+        Long idSurvey = 1L;
+        List<Choice> choices = ChoiceService.getChoiceById(idSurvey); //Récupération de tous les choix d'un sondage.
+        for( Choice c : choices )
+            Assert.assertEquals(c.getIdSurvey(),idSurvey); // Vérification si les choix correspondent au bon sondage.
+    }
+
+    /**
+     * Fonction getDeleteById
+     * Delete le choix
+     */
+    @Test
+    public void getDeleteById(){
+        Long id_choice = 2L;
+        ChoiceService.getDeleteById(id_choice);
+        List<Choice> choicesTest = ChoiceService.getChoiceById(id_choice);
+        for (Choice c: choicesTest)
+            Assert.assertNotEquals(c.getIdSurvey(), id_choice);
+    }
+
+    /**
+     * Fonction postChoiceById
+     * Ajoute un choix
+     */
+    @Test
+    public void  postChoiceById(){
+        //Variables
+        Long surveyID = 1L;
+        Timestamp date = Timestamp.valueOf("2020-06-11 12:22:44");
+
+        //Ajout d'un choix
+        Choice choice = ChoiceService.postChoiceById(date,surveyID);
+
+        //Vérification des informations
+        List<Choice> choices = ChoiceService.getChoiceById(surveyID);
+
+        //Vérification des informations
+        for( Choice c : choices )
+            if(c.getDate().equals(date)) {
+                Assert.assertEquals(c.getIdSurvey(), surveyID);
+                Assert.assertEquals(c.getDate(), date);
+            }
+    }
+}
