066fa931ee4cbb1a836b6af3953786972b4490cd
Alexandre CONRAD <alexandre.conrad2@etu.univ-lorraine.fr>
2021-01-11 13:59:35 +0100

    [ADD] import io.swagger.model.Choice;

---
+++ src/test/java/ServiceAnalyticsTest.java
@@ -0,0 +1,54 @@
+import io.swagger.model.Choice;
+import io.swagger.service.AnalyticsService;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test en rapport avec la classe "AnalyticsService"
+ */
+public class ServiceAnalyticsTest {
+
+    /**
+     * Fonction findDateByMaybeAvailable
+     * Retourne la date qui dispose du plus de "Disponible" et "Peut-être"
+     */
+    @Test
+    public void findDateByMaybeAvailable(){
+
+        //Variables
+        Long surveyID = 1L;
+
+        //Meilleur choix pour le sondage 1 est le choice 1
+        Long choiceID = 1L;
+
+        //Récupération du meilleur choix
+        Choice c = AnalyticsService.findDateByMaybeAvailable(surveyID);
+
+        //Vérifications des informations
+        Assert.assertEquals(c.getIdSurvey(), surveyID);
+        Assert.assertEquals(c.getIdChoice(), choiceID);
+
+    }
+
+    /**
+     * Fonction findDateByAvailable
+     * Retourne la date qui dispose du plus de "Disponible"
+     */
+    @Test
+    public void findDateByAvailable(){
+
+        //Variables
+        Long surveyID = 1L;
+
+        //Meilleur choix pour le sondage 1 est le choice 1
+        Long choiceID = 1L;
+
+        //Récupération du meilleur choix
+        Choice c = AnalyticsService.findDateByAvailable(surveyID);
+
+        //Vérifications des informations
+        Assert.assertEquals(c.getIdSurvey(), surveyID);
+        Assert.assertEquals(c.getIdChoice(), choiceID);
+
+    }
+}
