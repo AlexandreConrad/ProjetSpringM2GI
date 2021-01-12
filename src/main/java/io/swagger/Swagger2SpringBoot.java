0e65439e336605fae0a532d80b626bde0b2a0018
Bir Vincent <vincentbir@hotmail.com>
2021-01-12 17:32:41 +0100

    suppression d'un bug

--- src/main/java/io/swagger/Swagger2SpringBoot.java
+++ src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -31,29 +31,7 @@ public class Swagger2SpringBoot implements CommandLineRunner {
 
     @Override
     public void run(String... arg0) {
-
-       /* if (OffsetDateTime.of(2020,10,15,15,15,10,100, ZoneOffset.UTC).isBefore(OffsetDateTime.now()))
-            System.out.print("CA MARCHE");
-        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
-            throw new ExitException();
-        }*/
-        //Variables
-        String name = "Test du sondage";
-        String description = "Test description";
-        OffsetDateTime dateTime = OffsetDateTime.of(2020,10,15,15,15,10,100, ZoneOffset.UTC);
-        //Timestamp date = Timestamp.valueOf(dateTime.toString());
-
-        //Création d'un sondage
-        Sondage survey = new Sondage();
-        survey.setEndDate(dateTime);
-        survey.setName(name);
-        survey.setDescription(description);
-
-        //Fonction createSurvey
-        System.out.print("\n" + SurveyService.createSurvey(survey).getEndDate() +"\n");
-
-
-    }
+           }
 
     class ExitException extends RuntimeException implements ExitCodeGenerator {
         private static final long serialVersionUID = 1L;
